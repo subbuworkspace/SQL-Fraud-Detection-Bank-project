248ca917dbff36f02fe941734bf86812f1f4e4ad
subbuworkspace <111069112+subbuworkspace@users.noreply.github.com>
2025-03-03 23:52:46 +0530

    Add files via upload

---
+++ Data Analysis.sql
@@ -0,0 +1,43 @@
+Use FraudDetection
+select*from [dbo].[FraudData]
+
+--Exploratory Data Analysis (EDA)
+--Total Transactions and Fraud Distribution
+
+SELECT 
+    COUNT(*) AS Total_Transactions,
+    SUM(CAST(isFraud AS INT)) AS Total_Fraud,
+    (SUM(CAST(isFraud AS INT)) * 100.0 / COUNT(*)) AS Fraud_Percentage
+FROM  FraudData;
+
+--Fraud by Transaction Type
+
+SELECT 
+    type,
+    COUNT(*) AS Total_Transactions,
+    SUM(CAST(isFraud AS INT)) AS Fraud_Count,
+    (SUM(CAST(isFraud AS INT)) * 100.0 / COUNT(*)) AS Fraud_Rate
+FROM FraudData
+GROUP BY type
+ORDER BY Fraud_Rate DESC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
