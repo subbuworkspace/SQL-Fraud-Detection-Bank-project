03d77d36a0b87ac1e6fedfc9a18a710acc442565
subbuworkspace <111069112+subbuworkspace@users.noreply.github.com>
2025-04-03 23:49:16 +0530

    Update Data Analysis.sql

--- Data Analysis.sql
+++ Data Analysis.sql
@@ -12,7 +12,14 @@ FROM  FraudData;
 
 --Fraud by Transaction Type
 
-
+SELECT 
+    type,
+    COUNT(*) AS Total_Transactions,
+    SUM(CAST(isFraud AS INT)) AS Fraud_Count,
+    (SUM(CAST(isFraud AS INT)) * 100.0 / COUNT(*)) AS Fraud_Rate
+FROM FraudData
+GROUP BY type
+ORDER BY Fraud_Rate DESC;
 
 --Average Amount for Fraud vs. Non-Fraud
 
